e0163835c1b7387aca79bef0e4ef324605440696
MohamedElshadid <elshadid_mohamed@yahoo.com>
2020-06-24 07:21:52 +0200

    Vue Router and Initial Components

---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-toolbar dense>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>Bitfumes</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Forum</v-btn>
+        <v-btn text>Ask Question</v-btn>
+        <v-btn text>Category</v-btn>
+        <v-btn text>Login</v-btn>
+      </v-toolbar-items>
+
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
